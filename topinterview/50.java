5a8a6f2d9feaaf598c2d5868b5e16f2c06b4d804
null2p <68416208+null2p@users.noreply.github.com>
2023-02-15 00:56:19 +0900

    Update 50.java

--- topinterview/50.java
+++ topinterview/50.java
@@ -1,19 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
         double result = 1;
-        x = n < 0 ? 1/x : x;
+        x = n < 0 ? 1.0/x : x;
         n = n < 0 ? -n : n ;
 
         if(n==0||x==1){
             return 1;
         }
 
-        while(n > 0){
+        while(n != 0){
             if(n % 2 == 1){
                 result = result*x;
             }
             x = x*x;
-            n = n>>1;
+            n = n>>>1;
         }
 
         return result;
