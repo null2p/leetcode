dd762689361855fce32e43a3a8b06254927f125a
null2p <68416208+null2p@users.noreply.github.com>
2023-02-23 23:30:37 +0900

    [fix] 돌아가는 코드로 변경
    - 다만 너무 복잡하기 때문에 간단하게 바꾸는 과정 필요
    - 풀이의 논리과정은 이전과 거의 동일하다
    - 가장 큰 차이점은 for iteration의 i값을 reset해주는 경우가 존재한다는 것 : left cursor를 옮겼음에도 tCount == tSize 라면 i를 reset

--- topinterview/76.java
+++ topinterview/76.java
@@ -13,12 +13,16 @@ class Solution {
 
         List<Integer> tIdx = new ArrayList<>();
         int tSize = t.length();
-        int left = 0, right = 0, tCount = 0;
+
+        int left = 0, right = s.length(), tCount = 0;
+        
         String result = "";
         int resultSize = s.length();
 
         for(int i = 0 ; i < s.length() ; i++){
             Character sChar = s.charAt(i);
+            // System.out.println("s : "+sChar+", idx :"+i);
+
             if(tMap.get(sChar) == null){
                 continue;
             }
@@ -27,41 +31,62 @@ class Solution {
 
                 //init left
                 if(tCount == 0){
+                    // System.out.println("INIT");
                     left = i;
-                    // tCount += 1;
                     tIdx.remove(0);
                 }
 
-                //exclude duplicated counting
-                if(tMap.get(sChar) > 0){
-                    tCount+=1;
-                    tMap.put(sChar,tMap.remove(sChar)-1 );
-                    //if it is, set the location of right cursor
-                    if(tCount >= tSize){
+                if(tMap.get(sChar) != null){
+
+                    if(tMap.get(sChar)>0){
+                        tCount+=1;
+                    }
+                    
+                    if(right == i){
+                        tIdx.remove(tIdx.size()-1);
+                    }
+                    else{
+                        tMap.put(sChar,tMap.remove(sChar)-1);
+                    }
+                    //if it satisfies substring, set the location of right cursor
+                    if(tCount == tSize){
                         right = i;
 
                         //if the size of substring is min, get result String 
                         if(right - left + 1 <= resultSize){
                             result = s.substring(left,right+1);
-                            // System.out.println(result);
+                            resultSize = right - left + 1;
+                            // System.out.println("new result : "+result);
                         }
-                        //reset tMap key, value which is based on left cursor
+                        //reset tMap(key, value) on left cursor
+                        // System.out.print("reset Left : "+s.charAt(left)+" at "+left);
+
                         tMap.put(s.charAt(left),tMap.remove(s.charAt(left))+1);
+
+                        // System.out.print("(tMap value : "+tMap.get(s.charAt(left))+")");
+
+                        if(tMap.get(s.charAt(left)) >= 1){
+                            tCount-=1;
+                        }
+                        else{
+                            // System.out.print("!i is restored!");
+                            i-=1;
+                        }
+                        
                         if(tIdx.size()>0){
                             left = tIdx.remove(0);
+                            // System.out.print(" ---> new Left :"+s.charAt(left)+" at "+left);
+                            // System.out.println("(tMap value : "+tMap.get(s.charAt(left))+")");
+
                         }
 
-                        tCount-=1;
+                        // System.out.println("------tMap-----");
+                        // System.out.print(" a.get : "+tMap.get('a'));
+                        // System.out.print(", c.get : "+tMap.get('c'));
+                        // System.out.println(", e.get : "+tMap.get('e'));
+                        // System.out.println("---------------");
                     }
-                }
-                else if(tMap.get(sChar)==0){ // if we met duplicated sChar, reset left cursor
-                    if( s.charAt(tIdx.get(0)) != sChar){
-                        left = tIdx.remove(0);
-                    }
-                    else{
-                        tIdx.remove(0);
-                        left = i;
-                    }
+
                 }
             }
         }
