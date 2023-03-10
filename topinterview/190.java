81d397c04db9c09ce9d8b7adbb6e84d1a6fa34f6
null2p <68416208+null2p@users.noreply.github.com>
2023-03-10 11:07:38 +0900

    [feat] 190번 문제풀이

---
+++ topinterview/190.java
@@ -0,0 +1,20 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        if (n==0){
+            return 0;
+        }
+        int result = 0;
+
+        for(int i = 0 ; i<32 ; i++){
+            result = result << 1;
+            if((n&1)==1){
+                result += 1;
+            }
+            n = n>>1;
+
+        }
+
+        return result;
+    }
+}
